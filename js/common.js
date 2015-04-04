baf3c06e68c85117531746b374aefcc0e7ff4279
James Conkling <james.lane.conkling@gmail.com>
2015-04-04 18:10:10 -0400

    improve naming of tutorial html elements

--- js/common.js
+++ js/common.js
@@ -8,27 +8,27 @@
       // set tutorial sections initial hide state
       this.hideTutorialSections();
 
-      $('#tutorial .show-tutorial').one('click', this.showTutorial);
+      $('#tutorial .tutorial-head').one('click', this.showTurorial);
       $('#tutorial .close').on('click', this.hideTutorial);
 
     },
 
-    showTutorial: function(e){
+    showTurorial: function(e){
       e.preventDefault();
       e.stopPropagation();
 
-      var showTutorial = $(this),
-          sectionContainer = showTutorial.siblings('.section-container'),
-          tutorialContainer = showTutorial.parent('#tutorial'),
-          tutorialSection = sectionContainer.find('section');
+      var tutorialHead = $(this),
+          tutorialBody = tutorialHead.siblings('.tutorial-body'),
+          tutorialContainer = tutorialHead.parent('#tutorial'),
+          tutorialSection = tutorialBody.find('section');
 
       tutorialContainer.addClass('active');
 
-      // hide showTutorial
-      showTutorial.animate({opacity: 0}, 400);
+      // hide tutorialHead
+      tutorialHead.animate({opacity: 0}, 400);
 
-      // expand sectionContainer and fade in
-      sectionContainer.animate({height: '500px', opacity: 1}, 400, function(){
+      // expand tutorialBody and fade in
+      tutorialBody.animate({height: '500px', opacity: 1}, 400, function(){
         // slide in first tutorial section
         $(this).find('section[data-index="1"]').animate({left: 0, opacity: 1, zIndex: 0}, 200)
       });
@@ -40,22 +40,22 @@
 
       var $this = $(this),
           tutorialContainer = $this.parents('#tutorial'),
-          sectionContainer = $this.parents('.section-container'),
-          showTutorial = sectionContainer.siblings('.show-tutorial');
+          tutorialBody = $this.parents('.tutorial-body'),
+          tutorialHead = tutorialBody.siblings('.tutorial-head');
 
       tutorialContainer.removeClass('active');
 
       // show showTurorial
-      showTutorial.animate({opacity: 1}, 400);
+      tutorialHead.animate({opacity: 1}, 400);
 
-      // vertically collapse sectionContainer and fade out
-      sectionContainer.animate({height: 0, opacity: 0}, 400, function(){
+      // vertically collapse tutorialBody and fade out
+      tutorialBody.animate({height: 0, opacity: 0}, 400, function(){
         // return all tutorial sections to initial hide states
         app.hideTutorialSections();
       })
 
-      // re-bind click event handler on .show-tutorial
-      showTutorial.one('click', app.showTutorial);
+      // re-bind click event handler on .tutorial-head
+      tutorialHead.one('click', app.showTurorial);
     },
 
     hideTutorialSections: function(){
