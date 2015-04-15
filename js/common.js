772118154bb49943cb8b40f348809ffc6ce16240
James Conkling <james.lane.conkling@gmail.com>
2015-04-15 19:51:39 -0400

    refactor tutorial

--- js/common.js
+++ js/common.js
@@ -3,152 +3,142 @@
 (function(){
 
   var app = {
+    WINDOW: {
+      height: null,
+      width: null
+    },
+
+    ANIMATION: {
+      slide: 500,
+      scroll: 400
+    },
+
     initCommon: function(){
-      // set header height
-      this.sizeHeader();
+      this.resizeWindow();
 
       // set tutorial sections initial hide state
-      this.hideTutorialSections($('#tutorial .tutorial-body'));
+      this.hideTutorialPages();
 
-      $('#tutorial .tutorial-head').on('click', this.showTurorial);
-      $('#tutorial .close').on('click', this.hideTutorial);
-      $('#tutorial .advance-section').on('click', this.advanceTutorialSections);
-      $(window).on('resize', this.sizeHeader);
-
-    },
-
-    sizeHeader: function(e){
-      $('.header.header-home').height(function(){
-        return $(window).height() - $('#tutorial').outerHeight();
+      $('header nav').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        app.showTutorial();
       });
-    },
-
-    showTurorial: function(e){
-      e.preventDefault();
-      e.stopPropagation();
-
-      var tutorialHead = $(this),
-          tutorialBody = tutorialHead.siblings('.tutorial-body'),
-          tutorialContainer = tutorialHead.parent('#tutorial'),
-          tutorialSection = tutorialBody.find('section'),
-          slideAnimationDuration = 500;
-
-      // short circuit if #tutorial is already active
-      if(tutorialContainer.hasClass('active')){ return false; }
-      tutorialContainer.addClass('active');
-
-      // scroll to tutorial, leaving offset on top for magellan header
-      $('html, body').animate({
-        scrollTop: tutorialContainer.offset().top - $('.menu').outerHeight()
-      }, slideAnimationDuration);
-
-      // expand tutorialBody and fade in
-      tutorialBody.css({display: 'block'});
-      tutorialBody.animate({height: '500px', opacity: 1}, slideAnimationDuration, function(){
-        app.showTurorialSection(1, this);
-      });
-
-      // hide tutorialHead
-      tutorialHead.animate({opacity: 0}, 400);
+      $('section.tutorial .advance-section').on('click', this.advanceTutorialSections);
+      $(window).on('resize', this.resizeWindow);
 
     },
 
-    hideTutorial: function(e){
-      e.preventDefault();
-      e.stopPropagation();
+    resizeWindow: function(){
+      app.WINDOW.height = $(window).height();
+      app.WINDOW.width = $(window).width();
 
-      var $this = $(this),
-          tutorialContainer = $this.parents('#tutorial'),
-          tutorialBody = $this.parents('.tutorial-body'),
-          tutorialHead = tutorialBody.siblings('.tutorial-head'),
-          slideAnimationDuration = 500;
+      $('section').add('header').css({ height: app.WINDOW.height });
+    },
 
-      tutorialContainer.removeClass('active');
+    showTutorial: function(){
+      var tutorial = $('section.tutorial'),
+          tutorialContainer = tutorial.find('.tutorial-container');
 
-      // scroll to top
-      $('html, body').animate({ scrollTop: 0 }, slideAnimationDuration);
+      tutorial.addClass('active');
+
+      // scroll to tutorial
+      $('html, body').animate({ scrollTop: tutorial.offset().top }, app.ANIMATION.scroll);
+
+      tutorial.animate({height: app.WINDOW.height, opacity: 1}, app.ANIMATION.scroll, function(){
+        app.showTutorialSection(1);
+      })
+
+    },
+
+    hideTutorial: function(){
+      var tutorial = $('section.tutorial'),
+          tutorialContainer = tutorial.find('.tutorial-container');
 
       // vertically collapse tutorialBody and fade out
-      tutorialBody.animate({height: 0, opacity: 0}, slideAnimationDuration, function(){
-        tutorialBody.css({display: 'none'});
-        // return all tutorial sections to initial hide states
-        app.hideTutorialSections(tutorialBody);
+      tutorial.animate({height: 0, opacity: 0}, app.ANIMATION.slide, function(){
+        tutorial.removeClass('active');
+        app.hideTutorialPages();
       });
-
-      // show .tutorial-head
-      tutorialHead.animate({opacity: 1}, 400);
     },
 
-    showTurorialSection: function(idx, context){
-      var section = $(context).find('section[data-index="' + idx + '"]'),
-          title = section.find('.section-title'),
-          body = section.find('.section-body'),
-          image = section.find('.section-image'),
-          navBox = $('#tutorial .tutorial-nav .box[data-index="' + idx + '"]');
+    showTutorialSection: function(idx){
+      var page = $('section.tutorial .tutorial-page[data-index="' + idx + '"]'),
+          title = page.find('.section-title'),
+          body = page.find('.section-body'),
+          image = page.find('.section-image'),
+          navBox = $('section.tutorial .tutorial-nav .box[data-index="' + idx + '"]');
 
-      section.css({zIndex: 0}).addClass('active');
+      page.css({zIndex: 0}).addClass('active');
       image.css({zIndex: 0, opacity: 1});
       title.add(body).animate({bottom: 0, opacity: 1}, 400);
       navBox.addClass('active');
     },
 
-    hideTutorialSections: function(context){
-      var sections = $(context).find('section'),
-          title = sections.find('.section-title'),
-          body = sections.find('.section-body'),
-          image = sections.find('.section-image');
+    hideTutorialPages: function(){
+      var pages = $('section.tutorial .tutorial-page'),
+          title = pages.find('.section-title'),
+          body = pages.find('.section-body'),
+          image = pages.find('.section-image');
 
-      sections.css({zIndex: -1}).removeClass('active');
+      pages.css({zIndex: -1}).removeClass('active');
       image.css({zIndex: -1, opacity: 0});
       title.add(body).css({bottom: -60, opacity: 0});
 
       // deactivate tutorial nav boxes
-      $(context).find('.tutorial-nav .box.active').removeClass('active');
+      $('section.tutorial .tutorial-nav .box.active').removeClass('active');
     },
 
     advanceTutorialSections: function(e){
-      var $this = $(this),
-          dir = $this.attr('data-dir'),
-          sections = $this.parents('.tutorial-body').find('section'),
-          sectionCount = sections.length,
-          section = sections.filter('.active'),
-          sectionIdx = parseInt(section.attr('data-index')),
-          title = section.find('.section-title'),
-          body = section.find('.section-body'),
-          image = section.find('.section-image'),
-          sectionAnimationDuration = 400;
+      var arrowContainer = $(this),
+          dir = arrowContainer.attr('data-dir'),
+          tutorialPages = $('section.tutorial .tutorial-page'),
+          pagesCount = tutorialPages.length,
+          currentPage = tutorialPages.filter('.active'),
+          currentPageIdx = parseInt(currentPage.attr('data-index')),
+          title = currentPage.find('.section-title'),
+          body = currentPage.find('.section-body'),
+          image = currentPage.find('.section-image');
 
-      if(dir === 'next' && sectionIdx !== sectionCount){
-        var newSectionIdx = sectionIdx + 1,
+      if(dir === 'next' && currentPageIdx === pagesCount){
+        // if on the last slide, advance to map
+        // $('html, body').animate({ scrollTop: $('.map-container').offset().top }, app.ANIMATION.scroll);
+        app.hideTutorial();
+      }else if(dir === 'prev' && currentPageIdx === 1){
+        // if on the first slide, hide tutorial
+        app.hideTutorial();
+        $('html, body').animate({ scrollTop: 0 }, app.ANIMATION.scroll);
+      }else if(dir === 'next'){
+        var newPageIdx = currentPageIdx + 1,
             animationDir = 1;
-      }else if(dir === 'prev' && sectionIdx !== 1){
-        var newSectionIdx = sectionIdx - 1,
+      }else if(dir === 'prev'){
+        var newPageIdx = currentPageIdx - 1,
             animationDir = -1;
       }else{
         return false;
       }
 
-      var newSection = sections.filter('[data-index="' + newSectionIdx + '"]'),
-          newTitle = newSection.find('.section-title'),
-          newBody = newSection.find('.section-body'),
-          newImage = newSection.find('.section-image');
+      var newPage = tutorialPages.filter('[data-index="' + newPageIdx + '"]'),
+          newTitle = newPage.find('.section-title'),
+          newBody = newPage.find('.section-body'),
+          newImage = newPage.find('.section-image');
 
-      title.add(body).animate({bottom: 60 * animationDir, opacity: 0}, sectionAnimationDuration, function(){
+      title.add(body).animate({bottom: 60 * animationDir, opacity: 0}, app.ANIMATION.scroll, function(){
         image.css({zIndex: -1, opacity: 0});
         newImage.css({zIndex: 0, opacity: 1});
-        section.css({zIndex: -1}).removeClass('active');
-        newSection.css({zIndex: 0}).addClass('active');
-        newTitle.add(newBody).animate({bottom: 0, opacity: 1}, sectionAnimationDuration, function(){
-          app.advanceTutorialNav(sectionIdx, newSectionIdx);
+        currentPage.css({zIndex: -1}).removeClass('active');
+        newPage.css({zIndex: 0}).addClass('active');
+        newTitle.add(newBody).animate({bottom: 0, opacity: 1}, app.ANIMATION.scroll, function(){
+          app.advanceTutorialNav(currentPageIdx, newPageIdx);
         });
       });
 
     },
 
-    advanceTutorialNav: function(oldIdx, newIdx, context){
-      var context = context || $('#tutorial .tutorial-nav');
-      $(context).find('.box[data-index="' + oldIdx + '"]').removeClass('active');
-      $(context).find('.box[data-index="' + newIdx + '"]').addClass('active');
+    advanceTutorialNav: function(oldIdx, newIdx){
+      var nav = $('section.tutorial .tutorial-nav');
+      nav.find('.box[data-index="' + oldIdx + '"]').removeClass('active');
+      nav.find('.box[data-index="' + newIdx + '"]').addClass('active');
     }
 
   };
